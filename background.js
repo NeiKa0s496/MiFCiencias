1f25bbf4cd87a20a9335171f67e71bdfd1ede7fa
Zerabalus <hildajoanacg@ciencias.unam.mx>
2025-07-05 23:03:14 -0600

    app works, no personalization

---
+++ background.js
@@ -0,0 +1,150 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'loading') {
+    if (
+      /https?:\/\/w(w|e)(w|b)\.fciencias\.unam\.mx\/docencia\/horarios\/202[0-9][0-9]/.test(
+        changeInfo.url
+      )
+    ) {
+      chrome.tabs.executeScript(null, { file: './foreground.js' });
+    }
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(sender);
+  firstNames = [];
+  lastNames = [];
+  for (professor of request.msg) {
+    firstNames.push(getFirstName(professor));
+    lastNames.push(getLastName(professor));
+  }
+  console.log(firstNames, lastNames);
+  fetchProfessorsInfo(firstNames, lastNames, sender.tab.id);
+});
+
+function getLastName(name) {
+  let arr = name.split(' ').splice(-2);
+  let lastName = arr[0] + ' ' + arr[1];
+  return lastName;
+}
+
+function getFirstName(name) {
+  let arr = name.split(' ');
+  let firstName = arr[0];
+  return firstName;
+}
+
+//Doesnt replace ñ
+function removeSpecialChars(name) {
+  let removed = name
+    .normalize('NFD')
+    .replace(
+      /([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
+      '$1'
+    )
+    .normalize();
+  return removed;
+}
+
+function getLastNameRegex(name, unicode) {
+  let removed = removeSpecialChars(name);
+  let arrAccent = unicode.split(' ');
+  let arrRemoved = removed.split(' ');
+  let optionalName =
+    '(' +
+    arrAccent[0] +
+    '|' +
+    arrAccent[0].toLowerCase() +
+    '|' +
+    arrRemoved[0] +
+    '|' +
+    arrRemoved[0].toLowerCase() +
+    '|' +
+    arrRemoved[0].toUpperCase() +
+    ')[^}]*(' +
+    arrAccent[1] +
+    '|' +
+    arrAccent[1].toLowerCase() +
+    '|' +
+    arrRemoved[1] +
+    '|' +
+    arrRemoved[1].toLowerCase() +
+    '|' +
+    arrRemoved[1].toLowerCase() +
+    ')';
+  return optionalName;
+}
+
+function transformSpecialChars(strings) {
+  var specialChars = [225, 233, 237, 243, 250, 193, 201, 205, 211, 218, 241];
+  var unicode = [];
+  for (string of strings) {
+    console.log('String before: ', string);
+    var stringUnicode = '';
+    for (idx in string) {
+      if (specialChars.includes(string.charCodeAt(idx))) {
+        stringUnicode += '\\\\u00' + string.charCodeAt(idx).toString(16);
+      } else {
+        stringUnicode += string[idx];
+      }
+    }
+    console.log('String unicode: ', stringUnicode);
+    unicode.push(stringUnicode);
+  }
+  return unicode;
+}
+
+function fetchProfessorsInfo(firstNames, lastNames, tabid) {
+  fetch('https://www.misprofesores.com/escuelas/Facultad-de-Ciencias-UNAM_2842')
+    .then(function (response) {
+      switch (response.status) {
+        // status "OK"
+        case 200:
+          return response.text();
+        // status "Not Found"
+        case 404:
+          throw response;
+      }
+    })
+    .then((template) => {
+      firstUnicode = transformSpecialChars(firstNames);
+      lastUnicode = transformSpecialChars(lastNames);
+      console.log(firstUnicode, lastUnicode);
+      profInfo = [];
+      for (i = 0; i < firstUnicode.length; i++) {
+        let regExString =
+          '\\{[^}]*(' +
+          firstUnicode[i] +
+          '|' +
+          firstUnicode[i].toLowerCase() +
+          '|' +
+          removeSpecialChars(firstNames[i]) +
+          '|' +
+          removeSpecialChars(firstNames[i]).toLowerCase() +
+          '|' +
+          removeSpecialChars(firstNames[i]).toUpperCase() +
+          ')[^}]*' +
+          getLastNameRegex(lastNames[i], lastUnicode[i]) +
+          '.*?\\}';
+        let regEx = new RegExp(regExString);
+        console.log(regEx);
+        // Try searching with regex
+        if (regEx.test(template)) {
+          let result = template.match(regEx);
+          console.log(result[0]);
+          profObject = JSON.parse(result[0]);
+          console.log(profObject);
+          profInfo.push(profObject);
+        } else {
+          profInfo.push(null);
+        }
+      }
+      console.log(profInfo);
+      chrome.tabs.sendMessage(tabid, { professors: profInfo });
+      chrome.tabs.insertCSS(null, { file: './styles.css' });
+    })
+    .catch(function (response) {
+      // "Not Found"
+      console.log(response.statusText);
+    });
+}
---
+++ content.js
@@ -0,0 +1,64 @@
+// On receiving prof info create info tables and append them
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  let tables = document.getElementsByTagName("table");
+  let professors = message.professors;
+  for (let j = 0; j < professors.length; j++) {
+    if (professors[j] === null) {
+      let error = document.createElement("div");
+      error.className = "failed";
+      error.innerText = "No se encontró o(╥﹏╥)o";
+      tables[j].after(error);
+      continue;
+    }
+    let info = document.createElement("div");
+    let cal = parseInt(professors[j].c);
+    if (cal >= 8) {
+      info.className = "info green";
+    } else if (cal >= 6) {
+      info.className = "info yellow";
+    } else {
+      info.className = "info red";
+    }
+    let name = document.createElement("div");
+    name.className = "name";
+    name.innerText = professors[j].n + " " + professors[j].a;
+    let grade = document.createElement("div");
+    grade.innerText = "calificación del prof: " + professors[j].c;
+    let comments = document.createElement("div");
+    comments.innerText = "comentarios: " + professors[j].m;
+    let bottom = document.createElement("div");
+    bottom.className = "bottom";
+    let plus = document.createElement("a");
+    plus.className = "plus";
+    plus.innerHTML = "ir &plus;";
+    plus.href = getProfURL(professors[j].n, professors[j].a, professors[j].i);
+    bottom.appendChild(plus);
+    info.appendChild(name);
+    info.appendChild(grade);
+    info.appendChild(comments);
+    info.appendChild(bottom);
+    tables[j].after(info);
+  }
+});
+
+// Turns prof info into valid URL
+function getProfURL(firstName, lastName, id) {
+  let first = removeSpecialChars(firstName);
+  let last = removeSpecialChars(lastName);
+  let name = first.split(" ");
+  name = name.concat(last.split(" "));
+  name = name.join("-");
+  let url = "https://www.misprofesores.com/profesores/" + name + "_" + id;
+  return url;
+}
+
+function removeSpecialChars(name) {
+  let removed = name
+    .normalize("NFD")
+    .replace(
+      /([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
+      "$1"
+    )
+    .normalize();
+  return removed;
+}
